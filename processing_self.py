953ba85df499d8523fb4b24e6b551953e9f6b2e3
louis-tomczyk <159663498+louis-tomczyk@users.noreply.github.com>
2024-03-20 14:48:37 +0100

    Update processing_self.py
    mimo {vae,cma} - CPE

--- processing_self.py
+++ processing_self.py
@@ -4,9 +4,9 @@
 #   Author          : louis tomczyk
 #   Institution     : Telecom Paris
 #   Email           : louis.tomczyk@telecom-paris.fr
-#   Arxivs          :
-#   Date            : 2023-03-04
-#   Version         : 1.0.0
+#   Arxivs          : 2023-03-04 (1.0.0) - creation
+#   Date            : 2023-03-20 - mimo {vae,cma} - CPE
+#   Version         : 1.1.0
 #   Licence         : cc-by-nc-sa
 #                     Attribution - Non-Commercial - Share Alike 4.0 International
 # 
@@ -36,7 +36,6 @@
 # ---------------------------------------------
 # %%
 
-
 #%%
 import numpy as np
 import torch
@@ -44,12 +43,13 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import time
 
-
 import lib_kit as kit
 import lib_misc as misc
 import lib_prop as prop
 import lib_txdsp as txdsp
-import lib_ai as ai
+import lib_txhw as txhw
+import lib_rxdsp as rxdsp
+import lib_general as gen
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -57,96 +57,106 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 #%%
 def processing_self(tx,fibre,rx,saving,flags):
-        
-    tx,fibre,rx = init_processing(tx,fibre,rx,device)
 
+    tx,fibre,rx = init_processing(tx,fibre,rx,device)
+    elapsed     = np.zeros((rx['Nframes'],1))
+    
     for frame in range(rx['Nframes']):
+        t = time.time()
         with torch.set_grad_enabled(True):
+            
             rx              = init_train(tx,rx,frame)
             tx              = txdsp.transmitter(tx,rx)
             tx,fibre,rx     = prop.propagation(tx,fibre,rx)
-            
-            for batchNo in range(rx["NBatchFrame"]):
-                rx          = kit.train_self(batchNo,tx,fibre,rx,flags)
-                rx,loss     = kit.loss_function_shaping(tx,rx)
-                ai.optimise(loss,rx['optimiser'])
-                
-            plot_loss_batch(rx,flags,saving,['kind','law',"std",'linewidth'],skip = 5)
-
+            rx,loss         = rxdsp.mimo(tx,rx,saving,flags)
         
         rx["H_est_l"].append(rx["h_est"].tolist())
 
+        rx      = rxdsp.SNR_estimation(tx,rx)
+        rx      = kit.CPE(rx)
+        rx      = kit.SER_estimation(tx,rx)
         
-        rx      = kit.SNR_estimation(tx,rx)
-        rx      = kit.SER_estimation(tx,rx,rx['out_train'],frame)
-        array   = print_results(loss,frame,fibre,rx,saving)
+        array   = print_results(loss,frame,tx,fibre,rx,saving)
+        elapsed[frame] = time.time() - t
         
-    # END ------- for frame in range(rx['Nframes']):
-    tx,fibre, rx = save_data(tx,fibre,rx,array,saving)
+    tx,fibre, rx = save_data(tx,fibre,rx,saving,array)
 
+    print('exe time = {} +/-  {}'.format(np.mean(elapsed),np.std(elapsed)))
     return tx,fibre,rx    
 
 
-
-
-
-
-
-
 ####################################################################################################
+#%% Nested Functions
 ####################################################################################################
-####################################################################################################
-####################################################################################################
-####################################################################################################
+
+
+
 
 
 #%%
-# ============================================================================================ #
-# ============================================================================================ #
-def print_results(loss,frame,fibre,rx,saving):
+def print_results(loss,frame,tx,fibre,rx,saving):
     
     SER_valid   = rx["SER_valid"]
     SERs        = rx["SERs"]
     Losses      = rx["Losses"]
-    SNRdB_est   = rx["SNRdB_est"][frame].item()  
-    SNRdBs      = rx["SNRdBs"]
 
-    lossk       = round(loss.item(),0)
-    
-    SERvalid0   = round(SER_valid[0,frame].item(),10)
-    SERvalid1   = round(SER_valid[1,frame].item(),10)
-    SERvalid2   = round(SER_valid[2,frame].item(),10)
-    SERvalid3   = round(SER_valid[3,frame].item(),10)
-    
-    SERsvalid   = np.array([SERvalid0,SERvalid1,SERvalid2,SERvalid3])
-    SERmeank    = round(np.mean(SERsvalid),10)
-
-    
-    Shifts      = np.array([rx['shift'][0].item(),rx['shift'][1].item()])
-    Shiftsmean = round(np.mean(Shifts),1)
-    
-    SNRdBk     = round(10*np.log10(SNRdB_est),5)
+    if rx["mimo"].lower() == "vae":
+        SNRdB_est   = rx["SNRdB_est"][frame].item()  
+        SNRdBs      = rx["SNRdBs"]
+        lossk       = round(loss.item(),0)
+        SNRdBk      = round(10*np.log10(SNRdB_est),5)
+        SNRdBs.append(SNRdBk)
+        SNRdBs      = misc.list2vector(SNRdBs)
+        SERvalid0   = round(SER_valid[0,frame].item(),15)
+        SERvalid1   = round(SER_valid[1,frame].item(),15)
+        SERvalid2   = round(SER_valid[2,frame].item(),15)
+        SERvalid3   = round(SER_valid[3,frame].item(),15)
         
-    SERs.append(SERmeank)
+        SERsvalid   = np.array([SERvalid0,SERvalid1,SERvalid2,SERvalid3])
+        SERmeank    = round(np.mean(SERsvalid),15)
+    
+        Shifts      = np.array([rx['shift'][0].item(),rx['shift'][1].item()])
+        Shiftsmean  = round(np.mean(Shifts),1)
+        
+        SERs.append(SERmeank)
+        SERs        = misc.list2vector(SERs)
+
+        
+    else:
+        if rx['Frame']>rx["FrameRndRot"]:
+            lossk   = torch.std(loss[-1][:]).item()
+        else:
+            lossk   = np.NaN
+
+
     Losses.append(lossk)
-    SNRdBs.append(SNRdBk)
+
     
     Iteration   = misc.linspace(1,len(Losses),len(Losses))
     Losses      = misc.list2vector(Losses)
-    SNRdBs      = misc.list2vector(SNRdBs)
-    SERs        = misc.list2vector(SERs)
-    array       = np.concatenate((Iteration,Losses,SNRdBs,SERs),axis=1)
+    
+    if rx['mimo'].lower() == "vae":
+        array   = np.concatenate((Iteration,Losses,SNRdBs,SERs),axis=1)
+    else:
+        # array   = np.concatenate((Iteration,Losses,SERs),axis=1)
+        array   = np.concatenate((Iteration,Losses),axis=1)
+        
     thetak      = fibre['thetas'][frame][1]+fibre['thetas'][frame][0]
         
-    print("frame %d"     % frame,
-      '--- loss = %.1f'      % lossk,
-      '--- SNRdB = %.2f'    % SNRdBk,
-      '--- Theta = %.2f'    % (thetak*180/np.pi),
-      '--- <SER> = %.2e'     % SERmeank,
-      '--- <shift> = %.1f'   % Shiftsmean,
-      '--- r = %i'           % rx['r']
-      )
-    
+    if rx["mimo"].lower() == 'vae':
+        print("frame %d"        % frame,
+          '--- loss = %.1f'     % lossk,
+          '--- SNRdB = %.2f'    % SNRdBk,
+          '--- Theta = %.2f'    % (thetak*180/np.pi),
+          '--- std(Phi) = %.1f' % (np.std(tx["PhaseNoise"][0,:,rx["Frame"]])*180/np.pi),
+          '--- <SER> = %.2e'    % SERmeank,
+          # '--- r = %i'          % rx['r']
+          )
+    else:
+        print("frame %d"        % frame,
+          '--- loss = %.3e'     % lossk,
+          # '--- <SER> = %.2e'    % SERmeank,
+          )    
     
     
     '''
@@ -163,52 +173,40 @@ def print_results(loss,frame,fibre,rx,saving):
     '''
     
     return array
-    
-
-
+# ============================================================================================ #
 
 #%%
-# ============================================================================================ #
-# ============================================================================================ #
-
-def save_data(tx,fibre,rx,array,saving):
+def save_data(tx,fibre,rx,saving,array):
     
     
     Thetas_IN   = np.array([fibre["thetas"][k][0] for k in range(rx["Nframes"])])
     Thetas_OUT  = np.array([fibre["thetas"][k][1] for k in range(rx["Nframes"])])
     thetas      = list((Thetas_IN+Thetas_OUT)*180/np.pi)
     Thetas      = misc.list2vector(thetas)
-    array       = np.concatenate((array,Thetas),axis=1)
-
+    array2      = np.concatenate((array,Thetas),axis=1)
     
-    misc.array2csv(array, saving["filename"],["iteration","loss","SNR","SER","Thetas"])
+    if rx["mimo"].lower() == "vae":
+        misc.array2csv(array2, saving["filename"],["iteration","loss","SNR","SER","Thetas"])
+    else:
+        misc.array2csv(array2, saving["filename"],["iteration","loss","Thetas"])
 
     tx          = misc.sort_dict_by_keys(tx)
     fibre       = misc.sort_dict_by_keys(fibre)            
     rx          = misc.sort_dict_by_keys(rx)
 
     return tx,fibre,rx
-
-
-
-
-
-
+# ============================================================================================ #
 
 #%%
-# ============================================================================================ #
-# intialisation of the channel as the identity (in our case (h0)) and modulation format
-# ============================================================================================ #
-# as for now, there is no PMD, CD : it will be added in the generate_data_shaping function
-# Bibliography: 
-# [1] Caciularu et al. ICC 2018, 10.1109/ICCW.2018.8403666
-# [2] You et al. TNN Vol9 No6 1998
-#
-# louis tomczyk modif: 2023-08-20
-
 def init_processing(tx,fibre,rx,device):
-    tx["Npolars"]           = 2
+    tx['Npolars']           = 2
     tx["Ntaps"]             = tx["Nsps"]*tx["NSymbTaps"]-1
+    tx["RollOff"]           = 0.1
+    tx['NSymbFrame']        = rx["NSymbFrame"]
+    # Nconv = NSymbFrame + some extra symbols necessary for edges management
+    tx["Nconv"]             = rx["NSymbFrame"]+tx["Ntaps"]+1
+    tx["Nsamp_up"]          = tx["Nsps"]*(tx["Nconv"]-1)+1
+    tx["Nsamp_gross"]       = tx["Nsamp_up"]-(tx["Ntaps"]-1)
     
     # Channel matrix initiatlisation: Dirac
     h_est                           = np.zeros([tx['Npolars'],tx['Npolars'],2,tx["Ntaps"]])
@@ -223,20 +221,27 @@ def init_processing(tx,fibre,rx,device):
     tx["fs"]                = tx["Rs"]*tx["Nsps"]           # [GHz]
     
     rx["h_est"]             = h_est
-    rx['BatchLen']          = np.floor(rx['NSymbFrame']/rx["NBatchFrame"]).astype("int")
+    rx["NBatchFrame"]       = np.floor(rx['NSymbFrame']/rx['BatchLen']).astype("int")
     rx["NsampBatch"]        = rx['BatchLen']* tx['Nsps']
     
-    rx["N_cut"]             = 10                            # number of symbols cut off to prevent edge effects of convolution
+    rx["N_cut"]             = 3# number of symbols cut off to prevent edge effects of convolution
     rx["Nsamp_rx"]          = tx["Nsps"]*rx["NSymbFrame"]
     
     
-    fibre                   = prop.set_thetas(tx,fibre,rx)  # polarisations rotation angles generation
+    fibre                   = prop.set_thetas(tx,fibre,rx)
+    tx                      = txhw.gen_phase_noise(tx,rx)
+    # fibre['phiIQ']          = np.array([0, 0], dtype=np.complex64)
     
     rx['net']               = kit.twoXtwoFIR(tx).to(device)
     rx['optimiser']         = optim.Adam(rx['net'].parameters(), lr = rx['lr'])
     rx['optimiser'].add_param_group({"params": rx["h_est"]}) 
     
     # initialisation of the outputs    
+    if rx["mimo"].lower() == "cma":
+        rx["out_cpe"] = dict()
+        for k in range(rx["Nframes"]-rx['FrameRndRot']):
+            rx['out_cpe'][str(k)] = dict()
+            
     rx["H_est_l"]           = []
     rx["H_lins"]            = []
     
@@ -250,85 +255,15 @@ def init_processing(tx,fibre,rx,device):
     rx['Pnoise_est']        = misc.my_zeros_tensor((tx["Npolars"],rx['Nframes']))
     rx["minibatch_real"]    = misc.my_zeros_tensor((tx["Npolars"],2,rx["NsampBatch"]))
     
-    
-
     tx      = misc.sort_dict_by_keys(tx)
     fibre   = misc.sort_dict_by_keys(fibre)
     rx      = misc.sort_dict_by_keys(rx)
     
     return tx,fibre,rx
-
+# ============================================================================================ #
 
 
 #%%
-# ============================================================================================ #
-# ============================================================================================ #
-def plot_loss_batch(rx,flags,saving,keyword,skip):
-    
-    # a tensor with GRAD on cannot be plotted, it requires first to put it in a normal array
-    # that's the purpose of DETACH
-    
-    # do not intervert SAVEFIG and SHOW otherwise the figure will not be saved
-
-    if flags['plot_loss_batch']:
-
-        x = [k for k in range(rx['batchNo'])]
-        y = rx['losses_subframe'][rx['Frame']][:rx['batchNo']].detach().numpy()        
-        
-        if rx['Frame'] >= rx['FrameRndRot']:
-            linestyle   = "solid"
-        else:
-            linestyle = ":"
-         
-        if rx['Frame'] == rx['FrameRndRot']:
-            plt.plot(x,y,linestyle=linestyle,linewidth = 5, color = 'k',label = 'frame {} - after channel 1st round'.format(rx["Frame"]))   
-         
-        elif rx['Frame']%skip == 0:
-            if rx["Frame"]<rx['FrameRndRot']:
-                plt.plot(x,y,linestyle=linestyle,label = 'frame {} - b4 channel'.format(rx["Frame"]))
-            else:
-                plt.plot(x,y,linestyle=linestyle,label = 'frame {} - after channel'.format(rx["Frame"]))
-
-        if rx['Frame'] == rx['Nframes']-1:
-            plt.xlabel("iteration")
-            plt.ylabel("loss")
-            plt.ylim([-200,350])
-
-            plt.legend()
-            
-            
-            if keyword != []:
-                patterns        = []
-                pattern_values  = []
-                
-                for kw in keyword:
-                    pattern_index   = misc.find_string(kw,saving['filename'])
-                    patterns.append(saving['filename'][pattern_index[0]:pattern_index[1]])
-                    pattern_values.append(saving['filename'][pattern_index[1]+1:pattern_index[1]+1+2])
-                
-                tname = 'loss in batches - '
-                for k in range(len(patterns)):
-                    tname = tname + "{}-{} -- ".format(patterns[k],pattern_values[k])
-                    
-                plt.title(tname)
-            # if keyword != []:
-            #     pattern_index   = misc.find_string(keyword,saving['filename'])
-            #     pattern         = saving['filename'][pattern_index[0]:pattern_index[1]]
-            #     pattern_value   = saving['filename'][pattern_index[1]+1:pattern_index[1]+1+2]
-                            
-                # plt.title("loss in the batches - {} = {}".format(pattern,pattern_value))
-            else:
-                plt.title("loss in the batches")
-                
-            output_file = "{}.png".format(saving['filename']+' loss_batch')
-            plt.savefig(output_file,bbox_inches='tight')
-            plt.show()
-
-
-#%%
-# ============================================================================================ #
-# ============================================================================================ #
-
 def init_train(tx,rx,frame):
     
     if rx["N_lrhalf"] > rx["Nframes"]:
@@ -337,12 +272,20 @@ def init_train(tx,rx,frame):
         rx['optimiser'].param_groups[0]['lr'] = rx["lr_scheduled"]
         
     rx['out_train']         = misc.my_zeros_tensor((tx["Npolars"],2*tx["N_amps"],rx["NSymbFrame"]))
-    rx['out_const']         = misc.my_zeros_tensor((tx["Npolars"],2,rx["NSymbFrame"]))
+    
+        
     rx['Pnoise_batches']    = misc.my_zeros_tensor((tx["Npolars"],rx["NBatchFrame"]))
     rx["Frame"]             = frame
-    rx['losses_subframe']   = misc.my_zeros_tensor((rx["Nframes"],rx['NBatchFrame']))
+    
+    if rx["mimo"].lower() == 'vae':
+        rx['out_const']             = misc.my_zeros_tensor((tx["Npolars"],2,rx["NSymbFrame"]))
+        rx['losses_subframe']       = misc.my_zeros_tensor((rx["Nframes"],rx['NBatchFrame']))
+        rx['DKL_subframe']          = misc.my_zeros_tensor((rx["Nframes"],rx['NBatchFrame']))
+        rx['Llikelihood_subframe']  = misc.my_zeros_tensor((rx["Nframes"],rx['NBatchFrame']))
+    
     
     rx['net'].train()
     rx['optimiser'].zero_grad()
     
     return rx
+# ============================================================================================ #
