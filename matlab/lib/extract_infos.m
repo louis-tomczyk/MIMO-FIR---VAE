9ad7601da1ca19527744e50b82a0a199397c1fe5
louis-tomczyk <159663498+louis-tomczyk@users.noreply.github.com>
2024-07-25 16:52:26 +0200

    custom carac

--- matlab/lib/extract_infos.m
+++ matlab/lib/extract_infos.m
@@ -3,8 +3,8 @@
 %   Author          : louis tomczyk
 %   Institution     : Telecom Paris
 %   Email           : louis.tomczyk@telecom-paris.fr
-%   Date            : 2024-07-15
-%   Version         : 1.0.5
+%   Version         : 1.0.7
+%   Date            : 2024-07-25
 %   License         : cc-by-nc-sa
 %                       CAN:    modify - distribute
 %                       CANNOT: commercial use
@@ -16,7 +16,9 @@
 %   2024-07-10  (1.0.2) caps: sorting the struct
 %   2024-07-11  (1.0.3) phase noise management
 %   2024-07-12  (1.0.4) phase noise management --- for rx['mode'] = 'pilots'
-%   2024-07-15  (1.0.5) multiple files processing
+%   2024-07-16  (1.0.5) multiple files processing
+%   2024-07-23  (1.0.6) ThStd -> Th_std
+%   2024-07-25  (1.0.7) custom what_carac
 %
 % ----- MAIN IDEA -----
 % ----- INPUTS -----
@@ -72,52 +74,63 @@ caps.NBatches.Frame     = double(data.NBatchesFrame);
 caps.NBatches.Channel   = double(data.NBatchesChannel);
 
 if isfield(data, 'Phis_gnd') && ~sum(sum(isnan(data.Phis_gnd)))
-    if ~(strcmpi(caps.rx_mimo ,'cma') && strcmpi(caps.rx_mode,'blind'))
-        caps.phis_est           = 1;
-        caps.Batches.array      = linspace(1,caps.NBatches.Channel,caps.NBatches.Channel);
+    if ~strcmpi(caps.rx_mode,'blind') || strcmpi(caps.rx_mimo,'vae')
+        caps.phis_est   = 1;
     else
-        caps.phis_est           = 0;
-        caps.Batches.array      = NaN;
+        caps.phis_est   = 0;
     end
 else
-    caps.phis_est           = 0;
-    caps.Batches.array      = NaN;
+    caps.phis_est       = 0;
 end
 
 
-if ~strcmpi(caps.rx_mode,'pilots')
+if strcmpi(caps.rx_mode,'blind')
     caps.NBatches.Training  = caps.NFrames.Training*caps.NBatches.Frame;
     caps.NBatches.Channel   = caps.NBatches.Frame*caps.NFrames.Channel;
-
 else
     caps.NBatches.FrameCut  = caps.NBatches.Frame-2;
     caps.NBatches.Channel   = caps.NBatches.FrameCut*caps.NFrames.Channel;
     caps.NBatches.Training  = caps.NBatches.FrameCut*caps.NFrames.Training;
+end
 
+
+
+if caps.phis_est
+    caps.Batches.array      = linspace(1,caps.NBatches.Channel,caps.NBatches.Channel);
+else
+    caps.Batches.array      = NaN;
 end
 
 
 if ~isfield(caps.plot.phis,'pol')
-    caps.plot.phis.pol = 1;
+    caps.plot.phis.pol      = 1;
 end
 
 % fir
-caps.FIR.length         = double(data.NspTaps);
-caps.FIR.taps           = linspace(1,caps.FIR.length,caps.FIR.length)-ceil(caps.FIR.length/2);
+caps.FIR.length             = double(data.NspTaps);
+caps.FIR.tap                = ceil(caps.FIR.length/2);
+caps.FIR.taps               = linspace(1,caps.FIR.length,caps.FIR.length)-caps.FIR.tap;
 
 
 % caracs
 what_carac         = string({''});
 
-if strcmpi(data.ThLaw,'lin')
-    what_carac    = [what_carac,string({'ThEnd','Sth'})];
-elseif strcmpi(data.ThLaw,'gauss')
-    disp("Warning: all possible models implemented in Python's simulator" + ...
-        "are not taken into account. If not LINEAR model, Rwalk-Gauss by" + ...
-        "default is considered. Future versions may need to take it into" + ...
-        "account")
-    what_carac    = [what_carac,string({'Thstd'})];  % {dnu, Sth, Sph, ThEnd, PhEnd,Thstd, Phstd}
+
+if ~isfield(caps,'what_carac')
+    if strcmpi(data.ThLaw,'lin')
+        what_carac    = [what_carac,string({'ThEnd','Sth'})];
+    elseif strcmpi(data.ThLaw,'gauss')
+        disp("Warning: all possible models implemented in Python's simulator")
+        disp("are not taken into account. If not LINEAR model, Rwalk-Gauss by")
+        disp("default is considered. Future versions may need to take it into")
+        disp("account")
+        what_carac    = [what_carac,string({'Th_std'})];  % {dnu, Sth, Sph, ThEnd, PhEnd,Thstd, Phstd}
+    end
+else
+    what_carac    = [what_carac,string({caps.what_carac})];
 end
+
+
 
 
 if caps.phis_est && strcmpi(data.PhLaw,'lin') && ~sum(sum(isnan(data.Phis_gnd)))
