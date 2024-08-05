69d685fcab27a73d33dece29e5bd2cf94e168693
louis-tomczyk <159663498+louis-tomczyk@users.noreply.github.com>
2024-08-05 18:15:29 +0200

    server automatic management

--- python/main.py
+++ python/main.py
@@ -1,10 +1,10 @@
 # ---------------------------------------------
-# ----- INFORMATIONS -----
+# ----- INFORMATIONS ----- 
 #   Author          : louis tomczyk
 #   Institution     : Telecom Paris
 #   Email           : louis.tomczyk@telecom-paris.fr
-#   Version         : 2.0.2
-#   Date            : 2024-07-25
+#   Version         : 2.0.3
+#   Date            : 2024-08-05
 #   License         : GNU GPLv2
 #                       CAN:    commercial use - modify - distribute -
 #                               place warranty
@@ -32,6 +32,7 @@
 #   2.0.0 (2024-07-12) - LIBRARY NAME CHANGED: LIB_GENERAL -> LIB_PLOT
 #   2.0.1 (2024-07-17) - saving elapsed time
 #   2.0.2 (2024-07-25) - server management
+#   2.0.3 (2024-08-05) - server management
 # 
 # ----- MAIN IDEA -----
 #   Simulation of an end-to-end linear optical telecommunication system
@@ -84,7 +85,12 @@ from datetime           import date
 pi = np.pi
 clc()
 np.set_printoptions(linewidth=160)
-server  = 1
+
+
+if "infres" in PWD(show = False):
+    server  = 1                                                                   # if = 1, cancels all the checks
+else:
+    server  = 0
 
 #%% ===========================================================================
 # --- PARAMETERS ---
@@ -173,8 +179,8 @@ tx["nu"]                        = 0.0254                                       #
 #                            ['cpr','rand',"same","same","4QAM",3,0]]
 
 
-rx["NSymbFrame"]        = int(12800)
-# rx['NSymbBatch']        = int(140)
+rx["NSymbFrame"]        = int(20000)
+
 
 
 
@@ -199,33 +205,30 @@ tx['pn_filt_par']       = {
         'adaptative'    : 0                                                    # {1 == yes, 0 ==  no}
 }
 
-# if tx['flag_phase_noise'] == 0:
-#     tx['dnu'] = 0
 
-# tx['get_exec_time']     = ['frame',[]]
+
+tx['get_exec_time']     = ['frame',[]]
 ###############################################################################
 ################################## RECEIVER ###################################
 ###############################################################################
 
-tx['flag_phase_noise']  = 0
+tx['flag_phase_noise']  = 1
 rx['mode']              = "blind"                                             # {blind, pilots}
 rx["mimo"]              = "vae"                                                # {cma,vae}
 paramFIRlen             = [7]
-paramNSbB               = list(np.linspace(250,50,1).astype(int))
-paramPHI                = [10e3]                                        # [Hz] laser linewidth
+paramNSbB               = [250]#list(np.linspace(50,250,5).astype(int))
+paramPHI                = [1e3]#,5e3,1e4,5e4,1e5]                                        # [Hz] laser linewidth
 
 
 
 rx['SNRdB']             = 25                                                   # {>0}
-# rx["lr"]                = 1e-5                                                 # {>0,<1e-2} {cma ~ 1e-5, vae ~ 5e-4}
+tx['NSymbTaps']         = paramFIRlen[0]
 
 
-# tauCoh                  = tx['Rs']/2/pi/tx['dnu']
-# print(tauCoh)
 
 
 if tx['Rs'] == 64e9:
-    rx["FrameChannel"]  = 2
+    rx["FrameChannel"]  = 10
     rx["SymbScale"]     = 100
 else:
     rx["FrameChannel"]  = 40
@@ -240,22 +243,23 @@ else:
 # if linear variations -------- [[theta_start],[theta_end],[slope]]
 # if polarisation linewdith --- [[std],[NFramesChannel]]
 
-# paramPOL                        = np.array([[0],[25],[1]])
-fibre['fpol']                   = 1e3
-paramPOL                        = np.array([[np.sqrt(2*pi*fibre['fpol']/tx['Rs'])],[2]])
-# paramPOL                        = np.array([[0],[20]])
+paramPOL                        = np.array([[0],[10],[1]])
+# tmp_fpol                        = list(np.sqrt(np.array([1e2,1e3,1e4,1e5])*2*pi/tx['Rs']))
+# tmp_nfms                        = list((np.array([20,20,20,20])/1).astype(int))
+
+# tmp_fpol                        = list(np.sqrt(np.array([1e3])*2*pi/tx['Rs']))
+# tmp_nfms                        = list((np.array([10])/1).astype(int))
+# paramPOL                        = np.array([tmp_fpol,tmp_nfms])
+# paramPOL                        = np.array([[0],[10]])
 
 # paramLR                         = np.array([10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,200,300,400,500])*1e-6
 
-if rx['mimo'].lower() == "cma":
-    paramLR                     = np.array([10])*1e-6                      # {>0,<1e-2} {cma ~ 1e-5, vae é 5e-4}
-else:
-    paramLR                     = np.array([500])*1e-6                     # {>0,<1e-2} {cma ~ 1e-5, vae é 5e-4}
 
 
 
 
-fibre['PMD']                    = 0.00                                         # [ps/sqrt(km)]
+
+fibre['PMD']                    = 0                                         # [ps/sqrt(km)]
 fibre['D']                      = 17                                           # [ps/nm/km]
 fibre['PMD_SI']                 = fibre['PMD']*1e-12/np.sqrt(1000)             # [s/sqrt(m)], 1e-12/np.sqrt(1000) = 1 [ps/sqrt(km)]
 fibre['D_SI']                   = fibre['D']*1e-6                              # [s/m/m]
@@ -264,15 +268,13 @@ c                               = 299792458                                    #
 
 nSymbResCD                      = 0
 beta2                           = -wavelength**2/2/pi/c*fibre['D_SI']          # [s²/rad/m]
-fibre['DistRes_SI']             = nSymbResCD/(tx["Rs"]**2)/abs(beta2)/2/pi     # [m]
+fibre['DistRes_SI']             = nSymbResCD**2/(tx["Rs"]**2)/abs(beta2)/2/pi  # [m]
 fibre['DistProp_SI']            = 1e4                                          # [m]
 
 
 
 fibre['tauCD']                  = np.sqrt(2*pi*fibre['DistRes_SI']*abs(beta2)) # differential group delay [s]
 fibre['tauPMD']                 = fibre['PMD_SI']*np.sqrt(fibre['DistProp_SI'])# PMD induced delay [s]
-
-# print(fibre['tauCD'])
 
 
 if len(paramPOL) == 2:
@@ -285,12 +287,17 @@ else:
 
 
 
+# {>0,<1e-2} {cma ~ 1e-5, vae é 5e-4}
 
+if rx['mimo'].lower() == "cma":
+    rx['lr']        = 100*1e-7
+else:
+    rx['lr']        = 750*1e-6
 
 #%% ===========================================================================
 # --- FUNCTIONS ---
 # =============================================================================
-def process_data(nrea,npol,nphi,tx,fibre,rx):
+def process_data(nrea,npol,nphi,nsnr,tx,fibre,rx):
         
     if len(paramPOL) == 3:      # linear evolution
         RangeTheta                  = paramPOL[1,npol] - paramPOL[0,npol]
@@ -303,62 +310,55 @@ def process_data(nrea,npol,nphi,tx,fibre,rx):
         fibre["ThetasLaw"]['End']   = (paramPOL[1,npol])*pi/180
         
     else:
-        NFrames_Channel                 = int(paramPOL[-1])
-        fibre["ThetasLaw"]['theta_std'] = paramPOL[npol][0]
-        fibre['fpol']                   = paramPOL[npol][0]
+        NFrames_Channel                 = int(paramPOL[-1][npol])
+        fibre["ThetasLaw"]['theta_std'] = paramPOL[0][npol]
+        fibre["ThetasLaw"]['fpol']      = paramPOL[0][npol]
+        fibre['fpol']                   = int(paramPOL[0][npol]**2/2/pi*tx['Rs'])
 
     rx["NFrames"]                       = rx["FrameChannel"] + NFrames_Channel
     tx["dnu"]                           = paramPHI[nphi]                       # [Hz]
+    rx['SNRdB']                         = paramSNR[nsnr]                                                   # {>0}
     tx,fibre, rx                        = set_Nsymbols(tx,fibre,rx)
     saving["filename"]                  = misc.create_xml_file(tx,fibre,rx,saving,nrea)
-    
-
     
     tx,fibre,rx                         = processing(tx,fibre,rx,saving)
         
     misc.save2mat(tx,fibre,rx,saving)
 
     if tx['flag_phase_noise'] == 0:
-        plot.y2_axes(saving,"iteration",'Thetas','SER',['svg'])
+        if rx['mimo'].lower() != "vae":
+            plot.y2_axes(saving,"iteration",'Thetas','SER',['svg'])
+        else:
+            plot.y3_axes(saving,"iteration",'Thetas','SNR','SER',['svg'])            
     else:
         plot.y3_axes(saving,"iteration",'Thetas','Phis','SER',['svg'])
-
-
-    close("all")
         
+    
+
     return tx,fibre,rx
 
 #%% ===========================================================================
 # --- LOGISTICS ---
 # =============================================================================
 
-Nrea = 5
+Nrea = 10
+paramSNR                = list(np.linspace(5,25,21).astype(int))
 
 for nrea in range(Nrea):
+    print(f"nrea = {nrea}")
+    for nsnr in range(len(paramSNR)):
+        print(f"\t\t\tSNR = {paramSNR[nsnr]}")
+        
+        for nphi in range(len(paramPHI)):
+            for nSbB in range(len(paramNSbB)):
     
-    for nphi in range(len(paramPHI)):
-        for nSbB in range(len(paramNSbB)):
-
-            rx['NSymbBatch']  = paramNSbB[nSbB]
-
-            for npol in range(len(paramPOL[0])):
-                
-                Nlr     = paramLR.shape[0]
-                Nfir    = len(paramFIRlen)
-            
-                for nfir in range(Nfir):
+                rx['NSymbBatch']  = paramNSbB[nSbB]
     
-                    for nlr in range(Nlr):
-
-                        print(f"rea={nrea}")                        
-                        rx["lr"]        = paramLR[nlr]
-                        tx["NSymbTaps"] = paramFIRlen[nfir]
+                for npol in range(len(paramPOL[0])):
                             
-                        tx,fibre,rx     = process_data(nrea,npol,nphi,tx,fibre,rx)
-                        
-                        if "thetadiffs" in fibre:
-                            del fibre['thetadiffs']
-                        
+                    tx,fibre,rx     = process_data(nrea,npol,nphi,nsnr,tx,fibre,rx)
+                           
+                       
                         
 cuda.empty_cache()
 
